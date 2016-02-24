c358af29acce56e2d70ad5235bb037541ea055be
marcwebdude <marcus.faye@yahoo.com>
2016-02-24 07:53:29 -0600

    added bootstrap template

---
+++ WebApplication3/WebApplication3/bootstrap3/js/Marcus-Custom.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $('#DeltaTopNavigation').append('<div class="customButton" id="quickLinksButton"><a href="#sideNavBox">Quick Links</a></div><div class="customButton" id="submitTicketButton"><a href="#">Submit a Ticket</a></div>');
+
+    $('#sideNavBox').addClass('hidden');
+
+    $(function () {
+
+        $('#quickLinksButton a').click(function (event) {
+            event.preventDefault();
+            var target = $(this).attr('href');
+            $(target).toggleClass('hidden');
+        });
+
+    });
+});
