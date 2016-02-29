76ad6172e03e3cf782c05d7c80bb8516a80125a1
marcwebdude <marcus.faye@yahoo.com>
2016-02-29 14:49:17 -0600

    newest changes

--- WebApplication3/WebApplication3/bootstrap3/js/Marcus-Custom.js
+++ WebApplication3/WebApplication3/bootstrap3/js/Marcus-Custom.js
@@ -1,27 +1,33 @@
 ﻿$(document).ready(function () {
-    //$('#DeltaTopNavigation').append('<div class="customButton" id="quickLinksButton"><a href="#">Quick Links</a></div><div class="customButton" id="submitTicketButton"><a href="#">Submit a Ticket</a></div>');
+    $('div#DeltaTopNavigation li.static span.menu-item-text').each(function () {
+        var text = $(this).html();
+        if (text === 'Quick Links') {
+            $(this).closest('li').attr('id', 'quickLinksButton').addClass('customButton');
+        }
+        if (text === 'Submit a Ticket') {
+            $(this).closest('li').attr('id', 'submitTicketButton').addClass('customButton');
+        }
+    })
 
     $('#sideNavContainer').hide(0);
 
     $(function () {
-        $('#quickLinksButton a').click(function (event) {
+        $('#quickLinksButton .menu-item').click(function (event) {
             event.preventDefault();
             var target = $('#sideNavContainer');
 
             if ($(target).css('display') === 'none') {
                 $(target).show('slide', { direction: 'up' }, 1000);
-                $('#quickLinksButton').css({ 'background': 'linear-gradient(to bottom right, rgb(125, 81, 34), rgb(125, 80, 23))', 'border-color': 'rgb(91, 42, 25) rgb(208, 178, 80) rgb(208, 178, 80) rgb(91, 42, 25)', 'color': '#bbb' });
+                $('#quickLinksButton .menu-item').css({ 'background': 'linear-gradient(to bottom right, rgb(121, 15, 11), rgb(222, 29, 24))', 'border-color': 'rgb(91, 11, 9) rgb(114, 15, 11) rgb(159, 22, 18) rgb(114, 15, 11)', 'color': '#bbb' });
             }
 
             else if($(target).css('display') === 'block') {
                 $(target).hide('slide', { direction: 'up' }, 1000);
-                $('#quickLinksButton').css({ 'background': 'linear-gradient(to bottom right, rgb(125, 80, 23), rgb(125, 81, 34))', 'border-color': 'rgb(208, 178, 80) rgb(91, 42, 25) rgb(91, 42, 25) rgb(208, 178, 80)', 'color': '#fff' });
+                $('#quickLinksButton .menu-item').css({ 'background': 'linear-gradient(to bottom right, rgb(221, 102, 99), rgb(200, 44, 39))', 'border-color': 'rgb(159, 22, 18) rgb(136, 17, 14) rgb(136, 17, 14) rgb(136, 17, 14)', 'color': '#fff' });
 
             }
         });
     });
-    
-    //$('#main #mainbody span#DeltaPlaceHolderMain').remove();
-    //$('#mainbody').append('<div id="rotator"><div class="row"><div class="col-md-12"></div></div></div>');
-    //$('#mainbody').append('<div id="announcements"><div class="row"><div class="col-md-12"></div></div></div>');
+
+    $('#sideNavRemoval').closest('.ms-rtestate-read.ms-rte-wpbox').css('display', 'none');
 });
